8cf7c1e9a8240c281bbcc6af1721aa1cd86f64ca
Ting <tchangysl@gmail.com>
2025-05-04 23:08:36 +0800

    Add background dots to Featured

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,10 +4,15 @@ import { SECTION_HEADERS } from '@/constants/sections'
 import PortfolioCard from '@/components/PortfolioCard'
 import { FEATURED_BASE } from '@/constants/featured'
 import Link from 'next/link'
+import '@/styles/featured.css'
 
 export default function Featured() {
   return (
-    <Container as="section" id={SECTION_HEADERS.featured.id}>
+    <Container
+      as="section"
+      id={SECTION_HEADERS.featured.id}
+      className="featured-background"
+    >
       <SectionHeader sectionHeaderMap={SECTION_HEADERS.featured} />
       <div className="flex flex-col gap-6">
         {FEATURED_BASE.map(
