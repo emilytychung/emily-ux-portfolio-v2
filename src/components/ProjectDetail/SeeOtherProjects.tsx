56332819529775bac7e9078d61bc87888d29b370
Ting <tchangysl@gmail.com>
2025-05-07 23:18:26 +0800

    Set up RWD for `SeeOtherProjects`

--- src/components/ProjectDetail/SeeOtherProjects.tsx
+++ src/components/ProjectDetail/SeeOtherProjects.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 import Container from '@/components/Container'
 import SectionHeader from '@/components/SectionHeader'
 import { DETAIL_SECTION_HEADERS } from '@/constants/sections'
 import SimplePortfolioCard from '@/components/PortfolioCard/SimplePortfolioCard'
 import { FEATURED_BASE } from '@/constants/featured'
+import { GALLERY } from '@/constants/gallery'
 
 interface SeeOtherProjectsProps {
   currentProjectTitle: string
 }
 
-// TODO: decide what projects are in the other projects list, come back after all data stucture is set out
-const TEMP_OTHER_PROJECTS = [...FEATURED_BASE]
+// TODO: decide what projects are in the other projects list, come back after all data structure is set out
+const TEMP_OTHER_PROJECTS = [...FEATURED_BASE, ...GALLERY]
 
 export default function SeeOtherProjects({
   currentProjectTitle,
@@ -22,15 +24,23 @@ export default function SeeOtherProjects({
   return (
     <Container>
       <SectionHeader sectionHeaderMap={DETAIL_SECTION_HEADERS.seeOther} />
-      {otherProjects.map((project) => (
-        <SimplePortfolioCard
-          key={project.title}
-          company={project.company}
-          title={project.title}
-          coverImage={project.coverImage}
-          // tags={project?.tags ? project.tags : undefined}
-        />
-      ))}
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {otherProjects.map((project) => (
+          // TODO: change out link for slug when all project details confirmed
+          <Link
+            key={project.title}
+            href={`/projects/${project.title.toLowerCase().replace(/\s+/g, '-')}`}
+            className="block h-full"
+          >
+            <SimplePortfolioCard
+              company={project.company}
+              title={project.title}
+              coverImage={project.coverImage}
+              tags={project?.tags ? project.tags : undefined}
+            />
+          </Link>
+        ))}
+      </div>
     </Container>
   )
 }
